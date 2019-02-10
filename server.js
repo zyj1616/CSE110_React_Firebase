c6dc1801808200bfc0c9d081ef55e3a171b99105
Yujie Zhou <y1zhou@ucsd.edu>
2019-02-09 21:43:59 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+var httpServer = http.createServer(app);
+var io = require('socket.io')(httpServer);
+
+app.use(function(req,res,next){
+    console.log("Received",req.method,
+            "request for response",req.path,
+            "from",req.ip);
+    next();
+})
+
+app.use(express.static('src'));
+
+httpServer.listen(3000, function () {
+    console.log("Listening on port 3000");
+});
+
+var numClients = 0;
+io.on('connection', function (client) {
+    console.log('Client', numClients++, 'connected.');
+
+    client.on('join', function (data) {
+        console.log(data);
+    });
+    
+    client.on("chat", function(msg) {
+        console.log(msg);
+        client.broadcast.emit('chat msg', msg);
+    });
+        
+});
+
+
+    
